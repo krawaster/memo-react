09402e9cc61f451a857931c1472554b7346bd153
yukra <yukra@ciklum.com>
2022-10-18 20:01:55 -0400

    first commit

---
+++ assets/controllers/memo_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    connect() {
+        // let dificulty = prompt("вибери складнісьть від 4 - 8")
+    }
+}
